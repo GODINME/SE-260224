980b019154e62d4a294dc62c44f1d2766663892d
PAUL OFFEI <poffei@st.ug.edu.gh>
2024-02-28 22:16:14 +0000

    change the js file to ts file

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,4 +1,5 @@
-(function () {
+(function(){
+
     /**
      * This function loads data asynchronously from the URL. The callback function get called
      * once the data is ready
@@ -7,31 +8,38 @@
      * @param {string} url
      * @param {function} callback
      */
-    function LoadData(method, url, callback) {
+    function LoadData(method: string, url: string, callback: Function): void {
         // step 1. Create an empty XHR object
         let XHR = new XMLHttpRequest();
+
         // step 2. Compose the reques
         XHR.open(method, url);
+
         // step 3. Send the request to the server
         XHR.send();
+
         // step 4. Setup an event listener
-        XHR.addEventListener("readystatechange", function () {
-            if (XHR.status == 200 && XHR.readyState == 4) {
+        XHR.addEventListener("readystatechange", function(){
+            if (XHR.status == 200 && XHR.readyState == 4){
                 callback(XHR.responseText);
             }
         });
     }
-    function Start() {
+
+    function Start(): void {
         console.log("App Started...");
+
         // One way of doing it
         // LoadData("GET", "./Data/contact.json", function(displayData){
         //     console.log(displayData);
         // });
+
         // Another way of doing it
-        $.getJSON("./Data/contact.json", function (dataSource) {
-            console.log(dataSource.contactList);
+        $.getJSON("./Data/contact.json", function(dataSource){
+            console.log(dataSource.contactList)
         });
     }
+
     window.addEventListener("load", Start);
+    
 })();
-//# sourceMappingURL=app.js.map
